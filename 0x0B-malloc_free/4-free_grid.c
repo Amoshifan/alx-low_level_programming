5e91be535cecce1d26fda1633667ebb87cf80e4c
Amos Aondover Kwaghfan <callamos88@gmail.com>
2023-01-09 16:04:30 -0800

    It's not bragging if you can back it up

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - removing memory allocation
+ * @grid: the multidimensional array
+ * @height: height of the array
+ */
+
+void free_grid(int **grid, int height)
+{
+	int index;
+
+	for (index = 0; index < height; index++)
+		free(grid[index]);
+
+	free(grid);
+}
